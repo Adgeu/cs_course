2d99cae729767f6c2781952f96b9e89c082fe770
Alexey <adgeu@mail.ru>
2020-08-17 21:59:51 +0300

    change

--- Lessons/1/ClassWork/ConsoleApp1/ConsoleApp1/Program.cs
+++ Lessons/1/ClassWork/ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,6 +6,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("First Commit");
             Console.WriteLine("Hello World!");
         }
     }
