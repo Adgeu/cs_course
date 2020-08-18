97224f694712fc229db3e18feea073925d4d7801
Alexey Faradjev <adgeu@mail.ru>
2020-08-18 12:04:41 +0300

    Home work of the 1st lesson added

--- Lessons/1/HomeWork/WelcomeScreen/WelcomeScreen/Program.cs
+++ Lessons/1/HomeWork/WelcomeScreen/WelcomeScreen/Program.cs
@@ -8,19 +8,12 @@ namespace WelcomeScreen
         static void Main(string[] args)
         {
             Console.Write("Welcome!\nPlease, enter your name: ");
-
             string sName = Console.ReadLine();
-
             Console.WriteLine("Wait a few seconds...\n");
-
             Thread.Sleep(5000);
-
             Console.WriteLine($"Hello, {sName}!\nWait a few more seconds...\n");
-
             Thread.Sleep(5000);
-
             Console.WriteLine("Oh... Damn it all! I`m tired, I`m muhozhuk!\nGoodbye!\n\nPress any key to close the program...");
-
             Console.ReadLine();
         }
     }
