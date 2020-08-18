6ec45afbb47911f2df7f1ed6e6e34c627f8e459a
Alexey Faradjev <adgeu@mail.ru>
2020-08-18 11:56:30 +0300

    HomeWork commit

---
+++ Lessons/1/HomeWork/WelcomeScreen/WelcomeScreen/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading;
+
+namespace WelcomeScreen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Welcome!\nPlease, enter your name: ");
+
+            string sName = Console.ReadLine();
+
+            Console.WriteLine("Wait a few seconds...\n");
+
+            Thread.Sleep(5000);
+
+            Console.WriteLine($"Hello, {sName}!\nWait a few more seconds...\n");
+
+            Thread.Sleep(5000);
+
+            Console.WriteLine("Oh... Damn it all! I`m tired, I`m muhozhuk!\nGoodbye!\n\nPress any key to close the program...");
+
+            Console.ReadLine();
+        }
+    }
+}
