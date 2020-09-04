4619d3db70dfe984e133b3a9e26a5d7b7f9c17db
Alexey Faradjev <adgeu@mail.ru>
2020-09-04 15:36:16 +0300

    Home work of the 5th lesson added

---
+++ Lessons/6/HomeWork/EvenNumbers/EvenNumbers/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace EvenNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var number = ReadNumber();
+
+            var evenCount = 0;
+            var dividend = number;
+            while (dividend > 0)
+            {
+                if (dividend % 10 % 2 == 0)
+                    evenCount++;
+                                 
+                dividend /= 10;            
+            }
+
+            Console.WriteLine($"В числе {number} содержится следующее количество четных цифр: {evenCount}.\nНажмите любую клавишу для выхода... ");
+        }
+
+        /// <summary>
+        /// Пользовательский ввод числа
+        /// </summary>
+        /// <returns>Введённое число</returns>
+        static int ReadNumber()
+        {
+            while (true)
+            {
+                try
+                {
+                    Console.Write("Введите положительное натуральное число не более 2 миллиардов: ");
+                    var number = int.Parse(Console.ReadLine());
+
+                    if (number > 0)
+                        return number;
+
+                    PrintError($"Введено неверное значение! Попробуйте ещё раз:");
+                }
+                catch (Exception e)
+                {
+                    PrintError($"Ошибка {e.GetType()}! Попробуйте ещё раз:");
+                }
+            }
+        }
+
+        /// <summary>
+        /// Вывод ошибки в консоль
+        /// </summary>
+        /// <param name="message">Описание ошибки</param>
+        static void PrintError(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(message);
+            Console.ResetColor();
+        }
+    }
+}
---
+++ Lessons/6/HomeWork/MegaBank/MegaBank/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace MegaBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var currentAmount = ReadNumber("Введите сумму первоначального взноса в рублях: ", true);
+            var percent = ReadNumber("Введите ежедневный процент дохода в виде десятичной дроби (1% = 0.01): ");
+            var desiredAmount = ReadNumber("Введите желаемую сумму накопления в рублях: ", true);
+
+            var requiredDays = 0;
+            for (; currentAmount < desiredAmount; requiredDays++)
+            {
+                currentAmount += currentAmount * percent;
+            }
+
+            Console.WriteLine($"\nНеобходимое количество дней для накопления желаемой суммы: {requiredDays}.\nНажмите любую клавишу для выхода…");
+        }
+
+        /// <summary>
+        /// Пользовательский ввод числа
+        /// </summary>
+        /// <param name="caption">Заголовок</param>
+        /// <param name="isRubles">Является ли введённое значение числом в рублях (дробная часть будет являться ошибкой)</param>
+        /// <returns>Введённое число</returns>
+        static double ReadNumber(string caption, bool isRubles = false)
+        {
+            while (true)
+            {
+                var number = 0.0;
+
+                Console.Write(caption);
+
+                bool result = double.TryParse(Console.ReadLine(), out number);
+                if (!result || (isRubles && number % 1 != 0) || number < 0)
+                {
+                    PrintError("Ошибка! Неправильный формат ввода!");
+                    continue;
+                }
+
+                return number;
+            }
+        }
+
+        /// <summary>
+        /// Вывод ошибки в консоль
+        /// </summary>
+        /// <param name="message">Описание ошибки</param>
+        static void PrintError(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(message);
+            Console.ResetColor();
+        }
+    }
+}
