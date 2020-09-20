687810e505d92f44d0c18767679658967ab23dee
Alexey Faradjev <adgeu@mail.ru>
2020-09-20 16:06:03 +0300

    Home work of the 10th lesson added

---
+++ Lessons/10/HomeWork/PersonList/PersonList/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace PersonList
+{
+    class Person
+    {
+        public string PersonName { get; set; }
+        public byte PersonAge { get; set; }
+
+        public byte AgeIn4Years => 
+            (byte)(PersonAge + 4);
+
+        public string PersonInfo => 
+            $"Name: {PersonName}, age in 4 years: {AgeIn4Years}";
+
+        public Person(string personName, byte personAge)
+        {
+            PersonName = personName;
+            PersonAge = personAge;
+        }
+    }
+}
---
+++ Lessons/10/HomeWork/PersonList/PersonList/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PersonList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var personList = ReadPersons(3);
+
+            foreach (var person in personList)
+                Console.WriteLine(person.PersonInfo);
+
+            Console.WriteLine("Press any key to continue..");
+        }
+
+        static List<Person> ReadPersons(int count)
+        {
+            var personList = new List<Person>();
+
+            for (int i = 0; i < count; i++)
+            {
+                string name;
+                byte age;
+
+                // Name Input
+                Console.WriteLine($"Enter the name of the {i + 1} person:");
+                while (true)
+                {
+                    name = Console.ReadLine();
+
+                    if (!string.IsNullOrEmpty(name) && !string.IsNullOrWhiteSpace(name))
+                        break;
+
+                    Console.WriteLine("Input Error! Please, try again:");
+                }
+
+                // Age Input
+                Console.WriteLine($"Enter the age of the {i + 1} person:");
+                while (true)
+                {
+                    var result = byte.TryParse(Console.ReadLine(), out age);
+
+                    if (result)
+                        break;
+
+                    Console.WriteLine("Input Error! Please, try again:");
+                }
+
+                personList.Add(new Person(name, age));
+            }
+
+            return personList;
+        }
+    }
+}
