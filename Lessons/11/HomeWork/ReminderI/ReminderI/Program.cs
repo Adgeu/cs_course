b49e287a16b5138993b9f357981a90ffcfaa16bd
Alexey Faradjev <adgeu@mail.ru>
2020-09-22 12:38:39 +0300

    Home work of the 11th lesson added

---
+++ Lessons/11/HomeWork/ReminderI/ReminderI/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Reminder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var reminder1 = new ReminderItem(DateTimeOffset.Parse("22.09.2020 8:00"), "ПОДЪЁМ!");
+            Console.WriteLine(reminder1.WriteProperties());
+
+            Console.WriteLine();
+
+            var reminder2 = new ReminderItem(DateTimeOffset.Parse("29.09.2020 00:00"), "Не забыть, что нужно лечь спать");
+            Console.WriteLine(reminder2.WriteProperties());
+        }
+    }
+}
