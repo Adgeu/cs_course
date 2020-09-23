0b5f36afe0eb1eb3f08b30161faebf13130df328
Alexey Faradjev <adgeu@mail.ru>
2020-09-23 21:29:48 +0300

    ClassWork

---
+++ Lessons/12/ClassWork/ClassWork_12/Document.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ClassWork_12
+{
+    class BaseDocument
+    {
+        public string Title { get; set; }
+        public string Number { get; set; }
+        public DateTimeOffset IssueDate { get; set; }
+        public virtual string Description =>
+            $"Title: {Title}\nNumber: {Number}\nIssueDate: {IssueDate}";
+
+        public BaseDocument(string title, string number, DateTimeOffset issueDate)
+        {
+            Title = title;
+            Number = number;
+            IssueDate = issueDate;
+        }
+
+        public void WriteToConsole() =>
+            Console.WriteLine(Description);
+    }
+
+    class Passport : BaseDocument
+    { 
+        public string Country{ get; set; }
+        public string PersonName { get; set; }
+        public override string Description =>
+            $"Title: {Title}\nNumber: {Number}\nIssueDate: {IssueDate}\nCountry: {Country}\nPersonName: {PersonName}";
+
+        public Passport(string number, DateTimeOffset issueDate, string country, string personName) :
+            base("Passport", number, issueDate)
+        {
+            Country = country;
+            PersonName = personName;
+        }
+
+        public void ChangeIssueDate(DateTimeOffset newIssueDate) =>
+            IssueDate = newIssueDate;
+    }
+}
---
+++ Lessons/12/ClassWork/ClassWork_12/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ClassWork_12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var docs = new BaseDocument[]
+            {
+                new BaseDocument("Document", "123", DateTimeOffset.Parse("01.01.2020")),
+                new Passport("456", DateTimeOffset.Parse("01.01.2019"), "Rus", "Some Name"),
+                new Passport("789", DateTimeOffset.Parse("01.07.2015"), "Rus", "Some Other Name")
+            };
+
+            foreach (var doc in docs)
+            {
+                if (doc is Passport passport)
+                    passport.ChangeIssueDate(DateTimeOffset.UtcNow);
+
+                doc.WriteToConsole();
+                Console.WriteLine();
+            }
+        }
+    }
+}
