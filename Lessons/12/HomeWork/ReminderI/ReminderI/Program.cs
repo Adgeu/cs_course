455369723f22b935cbe6e545821d6b679ff51d7a
Alexey Faradjev <adgeu@mail.ru>
2020-09-27 16:47:58 +0300

    Home work of the 12th lesson added

---
+++ Lessons/12/HomeWork/ReminderI/ReminderI/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Reminder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var reminderItems = new List<ReminderItem>
+            {
+                new ReminderItem(DateTimeOffset.Now.AddDays(1), "Практиковаться в программировании"),
+                new PhoneReminderItem(DateTimeOffset.Now.AddDays(2), "Практиковаться в сновидении", "777-77-77"),
+                new ChatReminderItem(DateTimeOffset.Now.AddDays(3), "Практиковаться в практиковании", "Иванов Иван Иванович", "nagibator666")
+            };
+
+            foreach (var reminder in reminderItems)
+                Console.WriteLine(reminder.WriteProperties() + "\n"); 
+        }
+    }
+}
---
+++ Lessons/12/HomeWork/ReminderI/ReminderI/ReminderItem.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Reminder
+{
+    class ReminderItem
+    {
+        public DateTimeOffset AlarmDate { get; set; }
+
+        public string AlarmMessage { get; set; }
+
+        public TimeSpan TimeToAlarm =>
+            DateTimeOffset.Now - AlarmDate;
+
+        public bool IsOutdated =>
+            TimeToAlarm >= TimeSpan.Zero;
+
+        public ReminderItem(DateTimeOffset alarmDate, string alarmMessage)
+        {
+            AlarmDate = alarmDate;
+            AlarmMessage = alarmMessage;
+        }
+
+        public virtual string WriteProperties() =>
+            $"{this.GetType().Name}\nAlarmDate: {AlarmDate}\nAlarmMessage: {AlarmMessage}\nTimeToAlarm: {TimeToAlarm}\nIsOutdated: {IsOutdated}";
+
+    }
+    
+    class PhoneReminderItem : ReminderItem
+    {
+        public string PhoneNumber { get; set; }
+
+        public PhoneReminderItem(DateTimeOffset alarmDate, string alarmMessage, string phoneNumber) :
+            base(alarmDate, alarmMessage)
+        {
+            PhoneNumber = phoneNumber;
+        }
+
+        public override string WriteProperties() =>
+            base.WriteProperties() + $"\nPhoneNumber: {PhoneNumber}";
+
+    }
+
+    class ChatReminderItem : ReminderItem
+    {
+        public string ChatName { get; set; }
+
+        public string AccountName { get; set; }
+
+        public ChatReminderItem(DateTimeOffset alarmDate, string alarmMessage, string chatName, string accountName) :
+            base(alarmDate, alarmMessage)
+        {
+            ChatName = chatName;
+            AccountName = accountName;
+        }
+
+        public override string WriteProperties() =>
+            base.WriteProperties() + $"\nChatName: {ChatName}\nAccountName: {AccountName}";
+    }
+}
